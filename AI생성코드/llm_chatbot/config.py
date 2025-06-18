a92b94c3c5aacbdce83d070b76b7533805184435
Leeseo An <drisjin3148@gmail.com>
2025-06-18 19:28:38 +0900

    Update config.py

--- AI생성코드/llm_chatbot/config.py
+++ AI생성코드/llm_chatbot/config.py
@@ -1,8 +1,4 @@
 import os
-from dotenv import load_dotenv
 
-# .env 파일 로드
-load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  # GitHub Actions 설정됨
 
-# 환경변수 불러오기
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
